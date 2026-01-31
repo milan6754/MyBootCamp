5272fc40ee1446acb50f2a16322489b3120707e0
milan6754 <thapamilanmgr12@gmail.com>
2026-01-31 16:35:57 +0400

    some mod

--- Day27.py
+++ Day27.py
@@ -79,3 +79,24 @@ words = ["madam", "racecar","hello"]
 
 for word in words:
     print(f"{word}:{is_paldrome(word)}")
+
+arr = [1, 2, 3, 4, 5]
+
+left = 0 
+right = len(arr)-1
+
+while left<right:
+    arr[left],arr[right] = arr[right],arr[left]
+
+    left +=1
+    right-=1
+
+print(arr)
+
+arr = [1, 2, 3, 4, 5]
+target = 6
+
+for i in range(len(arr)):
+    for j in range(len(arr)):
+        if arr[i] +arr[j]==target:
+            print(arr[i],arr[j])
